d9ae70c53ce1143b7366f9daef7ee195a9210765
hokawa281 <94590414+hokawa281@users.noreply.github.com>
2024-04-11 10:14:12 +0900

    テスト

--- FindFibonacciGcd.cpp
+++ FindFibonacciGcd.cpp
@@ -7,6 +7,15 @@ uint32_t gcd(uint32_t a, uint32_t b) {
 	return 0;
 }
 
+    // ユーグリッドの互除法
+    while ((r = a % b) != 0)  // yで割り切れるまでループ
+    {
+        a = b;
+        b = r;
+    }
+    return b;
+}
+
 uint32_t fibonacci(uint32_t n) {
 	return 0;
 }
